3519c9437915a60035b3dd695141d9d2bfdeabf9
MOHAMMAD SHADAB ABEDIN <shadababe04@gmail.com>
2024-09-10 00:08:51 +0530

    update the fgets function to take multi word name

--- Structure/1_struct.c
+++ Structure/1_struct.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 //TODO: 1.Define a structure Employee with member variables id, name, salary
-//TODO: 2.WAF to take input employee data from user
+
 
 struct Employee
 {
@@ -11,18 +11,28 @@ struct Employee
     int salary;
 };
 
-void getData()
+//TODO: 2.WAF to take input employee data from user
+struct Employee getData()
 {
     struct Employee E1;
-    printf("Enter the id");
+    printf("Enter the id ");
     scanf("%d",&E1.id);
-    printf("Enter the name");
-    scanf("%s",E1.name);
-    printf("Enter the Slary");
+    printf("Enter the name ");
+    //scanf("%s",E1.name);
+    getchar();
+    fgets(E1.name,sizeof(E1.name),stdin);
+    printf("Enter the Slary ");
     scanf("%d",&E1.salary); 
+    return E1;
 }
 int main() {
-    
+    struct Employee emp;
+    emp = getData();
+
+    printf("Display the details of employee:\n");
+    printf("Employee id : %d\n", emp.id);
+    printf("Employee name : %s",emp.name);
+    printf("Employee Salary :%d\n",emp.salary);
     
     return 0;
 }
