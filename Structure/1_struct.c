873b2ccaf1d647ad19d3b17d5f6002626b852e96
MOHAMMAD SHADAB ABEDIN <shadababe04@gmail.com>
2024-09-09 23:31:49 +0530

    WAP to define structure

---
+++ Structure/1_struct.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//TODO: Define a structure Employee with member variables id, name, salary
+
+struct Employee
+{
+    /* data */
+    int id;
+    char name;
+    int salary
+};
+
+
+int main() {
+    
+    
+    return 0;
+}
