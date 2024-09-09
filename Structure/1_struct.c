8b3c224849fcc0978d9fd84e12ca747d8d2b46a1
MOHAMMAD SHADAB ABEDIN <shadababe04@gmail.com>
2024-09-09 23:51:31 +0530

    WAF to get the data

--- Structure/1_struct.c
+++ Structure/1_struct.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 
-//TODO: Define a structure Employee with member variables id, name, salary
+//TODO: 1.Define a structure Employee with member variables id, name, salary
+//TODO: 2.WAF to take input employee data from user
 
 struct Employee
 {
     /* data */
     int id;
-    char name;
-    int salary
+    char name[20];
+    int salary;
 };
 
-
+void getData()
+{
+    struct Employee E1;
+    printf("Enter the id");
+    scanf("%d",&E1.id);
+    printf("Enter the name");
+    scanf("%s",E1.name);
+    printf("Enter the Slary");
+    scanf("%d",&E1.salary); 
+}
 int main() {
     
     
