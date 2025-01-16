ce8f4ed5a64a55134632db7a8773f42b9572fe40
MOHAMMAD SHADAB ABEDIN <shadababe04@gmail.com>
2025-01-16 16:35:14 +0530

    add static concept based

---
+++ 3_Class/static.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class item
+{
+    public:
+    int a, b;  //Instance Member Variable(IMV)
+    static int k; //Static Member Variable(SMV) class variable-share resource
+
+};
+int item::k;
+int main()
+{
+    item i1,i2;
+    i1.a=5;
+    cout<<i2.a<<endl;
+    //i2.k=6;
+    item::k=7;
+    cout<<i1.k;
+    return 0;
+}
